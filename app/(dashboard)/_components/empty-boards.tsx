07cc99b90aed6c1be396adbe829c0f8a64c199f8
Prabhat Kumar <pk993105@gmail.com>
2025-04-16 10:33:14 +0530

    Refactor EmptyBoards component and enhance create board mutation with user name validation

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -1,51 +1,42 @@
 "use client";
 
-import { Button } from '@/components/ui/button';
-import Image from 'next/image';
-import { toast } from 'sonner';
-import { useMutation } from 'convex/react';
-import { useOrganization } from '@clerk/nextjs';
-import { useApiMutation } from '@/hooks/use-api-mutation';
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { toast } from "sonner";
+import { useOrganization } from "@clerk/nextjs";
+import { useApiMutation } from "@/hooks/use-api-mutation";
 
-import { api } from '@/convex/_generated/api';
-
+import { api } from "@/convex/_generated/api";
 
 export const EmptyBoards = () => {
-    const { mutate, pending } = useApiMutation(api.board.create);
-    const { organization } = useOrganization();
+  const { mutate, pending } = useApiMutation(api.board.create);
+  const { organization } = useOrganization();
 
-    const onClick = () => {
-        if (!organization) return;
-        mutate({
-            orgId: organization.id,
-            title: "Untitled"
-        })
-          .then((id) => {
-            toast.success("Board Created");
-            //redirect  to board/ id
-          })
-           .catch(() => toast.error("Failed to create board"));
-    };
+  const onClick = () => {
+    if (!organization) return;
+    mutate({
+      orgId: organization.id,
+      title: "Untitled",
+    })
+      .then(() => {
+        toast.success("Board Created");
+        //redirect  to board/ id
+      })
+      .catch(() => toast.error("Failed to create board"));
+  };
 
-    return (
-        <div className = "h-full flex flex-col items-center justify-center">
-            <Image
-                src = "/note.png"
-                height = {120}
-                width = {120}
-                alt = "Empty Search"    
-            />
-            <h2 className = "text-2xl font-semibold mt-6">
-                Create your first board.
-            </h2>
-            <p className =" text-muted-foreground text-sm mt-2 mb-4">
-                Start by creating a board for your organization.
-            </p>
-            <div>
-                <Button disabled={pending} onClick={onClick} size = "lg">
-                    Create board
-                </Button>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className='h-full flex flex-col items-center justify-center'>
+      <Image src='/note.png' height={120} width={120} alt='Empty Search' />
+      <h2 className='text-2xl font-semibold mt-6'>Create your first board.</h2>
+      <p className=' text-muted-foreground text-sm mt-2 mb-4'>
+        Start by creating a board for your organization.
+      </p>
+      <div>
+        <Button disabled={pending} onClick={onClick} size='lg'>
+          Create board
+        </Button>
+      </div>
+    </div>
+  );
+};
--- convex/board.ts
+++ convex/board.ts
@@ -1,44 +1,51 @@
 import { v } from "convex/values";
 
 import { mutation } from "./_generated/server";
+import { toast } from "sonner";
 
 const images = [
-    "/placeholders/1.svg",
-    "/placeholders/2.svg",
-    "/placeholders/3.svg",
-    "/placeholders/4.svg",
-    "/placeholders/5.svg",
-    "/placeholders/6.svg",
-    "/placeholders/7.svg",
-    "/placeholders/8.svg",
-    "/placeholders/9.svg",
-    "/placeholders/10.svg",
+  "/placeholders/1.svg",
+  "/placeholders/2.svg",
+  "/placeholders/3.svg",
+  "/placeholders/4.svg",
+  "/placeholders/5.svg",
+  "/placeholders/6.svg",
+  "/placeholders/7.svg",
+  "/placeholders/8.svg",
+  "/placeholders/9.svg",
+  "/placeholders/10.svg",
 ];
 
-export const create = mutation ({
-    args: {
-        orgId: v.string(),
-        title: v.string(),
-    },
-    handler: async (ctx, args) => {
-        const identity = await ctx.auth.getUserIdentity();
+export const create = mutation({
+  args: {
+    orgId: v.string(),
+    title: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+    console.log(identity, "IDENTITY");
+    if (!identity) {
+      throw new Error("Unauthorized");
+    }
 
-        if (!identity) {
-            throw new Error("Unauthorized");
-        }
+    const randomImage = images[Math.floor(Math.random() * images.length)];
 
-        const randomImage = images[Math.floor(Math.random() * images.length)];
+    // console.log(identity, "IDENTITY");
 
-        console.log(randomImage,"TEST")
+    if (!identity.name) {
+      toast.error("Please set your name in your profile settings.");
+      return;
+    }
+    console.log(randomImage, "TEST");
 
-        const board = await ctx.db.insert("boards", {
-            title: args.title,
-            orgId: args.orgId,
-            authorId: identity.subject,
-            authorName: identity.name!,
-            imageUrl: randomImage,
-        });
+    const board = await ctx.db.insert("boards", {
+      title: args.title,
+      orgId: args.orgId,
+      authorId: identity.subject,
+      authorName: identity.name!,
+      imageUrl: randomImage,
+    });
 
-        return board;
-    }, 
+    return board;
+  },
 });
